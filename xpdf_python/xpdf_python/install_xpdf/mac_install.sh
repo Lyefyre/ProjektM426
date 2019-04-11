947515a901f53266ab948eb5e385c4a761e4daa7
botsi <david@abotsi.com>
2019-04-11 10:43:05 +0200

    Add files via upload

---
+++ xpdf_python/xpdf_python/install_xpdf/mac_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "INSTALLING XPDF"
+cd /tmp/
+wget https://xpdfreader-dl.s3.amazonaws.com/xpdf-tools-mac-4.01.tar.gz
+tar -xvzf xpdf-tools-mac-4.01.tar.gz
+cp xpdf-tools-mac-4.01/bin64/* /usr/local/bin
+cp xpdf-tools-mac-4.01/doc/sample-xpdfrc /usr/local/etc/xpdfrc
+
